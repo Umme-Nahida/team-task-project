7ea8dc41d0e02f7a0d4ae04566199d19baaf60ff
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-19 23:42:29 +0600

    added payment card and payment section

--- src/Pages/PaymentService/PaymentCard.jsx
+++ src/Pages/PaymentService/PaymentCard.jsx
@@ -3,7 +3,7 @@
 const PaymentCard = ({title,icon:Icon, des}) => {
     return (
         <div className=" p-10 rounded-sm border-l-4 border-blue-500 shadow-2xl">
-            <Icon></Icon>
+            <Icon className='text-xl'></Icon>
             <h1>{title} </h1>
             <p>{des} </p>
             
--- src/Pages/PaymentService/PaymentService.jsx
+++ src/Pages/PaymentService/PaymentService.jsx
@@ -1,23 +1,35 @@
+import { FaArrowRightLong } from "react-icons/fa6";
 import { services } from "./payment";
 import PaymentCard from "./PaymentCard";
 
 const PaymentService = () => {
-    return (
-        <div>
-            {/* payment service card */}
-            <div className="md:w-1/2 grid grid-cols-2 gap-5 ">
-                {services.map(item=>(
-                    <PaymentCard key={item.label} title={item.label} icon={item.icon} des={item.description}></PaymentCard>
-                ))}
-            </div>
+  return (
+    <div className="p-10 lg:mt-20 flex items-center justify-center gap-x-10">
+      {/* payment service card */}
+      <div className="md:w-1/2 grid grid-cols-2 gap-5 ">
+        {services.map((item) => (
+          <PaymentCard
+            key={item.label}
+            title={item.label}
+            icon={item.icon}
+            des={item.description}
+          ></PaymentCard>
+        ))}
+      </div>
 
-            {/* payment services text */}
-            <div className="md:w-1/2 bg-red-500">
-
-            </div>
-
+      {/* payment services text */}
+      <div className="md:w-1/2 bg-red-500 flex items-center justify-center  ">
+        <div className="w-[400px] space-y-3 -mt-72">
+          <h1 className="text-4xl font-bold text-[#857885]">Make & Share <span className="text-[#5C0E62]">Payments</span> With Community</h1>
+          <p className="">
+            Hac sem ut eget tellus diam convallis lacus a, interdum nulla sem
+            adipiscing inw
+          </p>
+          <button className="flex items-center gap-x-2 text-[#5C0E62] font-bold">Learn more <FaArrowRightLong /></button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PaymentService;
