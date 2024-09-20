4072e1b54b6e9b2885ec3e063d808c602268f5b4
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-20 11:15:46 +0600

    secure section added

--- src/Pages/SecureTransection/SecureTransaction.jsx
+++ src/Pages/SecureTransection/SecureTransaction.jsx
@@ -5,7 +5,7 @@ const SecureTransaction = () => {
         <div className="flex  items-center py-28">
         {/* content  */}
         <div className='md:w-2/5 space-y-4 lg:ml-20'>
-            <p className="text-red-500 font-medium">MOBILE TRANSACTIO</p>
+            <p className="text-red-500 font-medium">MOBILE TRANSACTION</p>
             <h1 className='text-6xl font-bold text-[#857885]'>Smart Mobile  <br /> <span className="text-[#5C0E62]">Banking</span></h1>
             <p className="lg:pr-32">Hac sem ut eget tellus diam convallis lacus a, interdum nulla sem adipiscing inw</p>
             <button className="flex items-center gap-x-2 text-[#5C0E62] font-bold">Learn more <FaArrowRightLong /></button>
